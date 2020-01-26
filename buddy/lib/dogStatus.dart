f33c2eeb07be16d8ba63c6105f6029cb9522dc34
Wahid Bawa <wahidbawa10@gmail.com>
2020-01-26 07:12:02 -0500

    before i break everything

--- buddy/lib/dogStatus.dart
+++ buddy/lib/dogStatus.dart
@@ -11,32 +11,32 @@ class dogStatus {
 
   Widget emoteBubble() {
     switch(emote){
-      case 0:{ // sad -> anger
+      case 1:{ // sad -> anger
         return Image.asset('assets/img/emote_faceAngry.png');
       }
       break;
       
-      case 1:{ // sad -> dazed/ confused
+      case 2:{ // sad -> dazed/ confused
         return Image.asset('assets/img/emote_swirl.png');
       }
       break;
 
-      case 2:{ // neutral -> sleep
+      case 3:{ // neutral -> sleep
         return Image.asset('assets/img/emote_sleeps.png');
       }
       break;
 
-      case 3:{ // neutral -> dots
+      case 4:{ // neutral -> dots
         return Image.asset('assets/img/emote_dots3.png');
       }
       break;
 
-      case 4:{ // happy -> happy
+      case 5:{ // happy -> happy
         return Image.asset('assets/img/emote_faceHappy.png');
       }
       break;
 
-      case 5:{ // happy -> heart
+      case 6:{ // happy -> heart
         return  Image.asset('assets/img/emote_heart.png');
       }
       break;
